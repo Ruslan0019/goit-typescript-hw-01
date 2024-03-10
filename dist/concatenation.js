87fd09f5cf1a0f740cd25c26487fcb1d985a3d96
Ruslan0019 <133685041+Ruslan0019@users.noreply.github.com>
2024-03-10 17:14:46 +0200

    /

---
+++ dist/concatenation.js
@@ -0,0 +1,4 @@
+export function concatenation(firstWorld, secondWorld) {
+    console.log(`${firstWorld} ${secondWorld}`);
+}
+//# sourceMappingURL=concatenation.js.map
---
+++ src/concatenation.ts
@@ -0,0 +1,4 @@
+export function concatenation(firstWorld: string, secondWorld: string) {
+    console.log(`${firstWorld} ${secondWorld}`);
+    
+}
